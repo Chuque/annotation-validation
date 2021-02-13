d6983adabb510f23e483a665eb5c61ddee2a4005
Rafael Chuque <Rafa_chuque@hotmail.com>
2021-02-13 14:26:43 -0300

    Add example methods and annotations

---
+++ src/main/java/com/chuque/annotationprocessing/annotation/TimedMillis.java
@@ -0,0 +1,12 @@
+package com.chuque.annotationprocessing.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TimedMillis {
+    //annotation marker to log method execution duration in milliseconds
+}
---
+++ src/main/java/com/chuque/annotationprocessing/core/Foo.java
@@ -0,0 +1,27 @@
+package com.chuque.annotationprocessing.core;
+
+import com.chuque.annotationprocessing.annotation.TimedMillis;
+import com.chuque.annotationprocessing.annotation.TimedSeconds;
+
+public class Foo {
+
+    public void bar(){
+        System.out.println("bar called");
+    }
+
+    @TimedMillis
+    public void baz(){
+        System.out.println("baz called");
+    }
+
+    @TimedSeconds
+    public void qux(){
+        System.out.println("qux called");
+    }
+
+    @TimedMillis
+    @TimedSeconds
+    public void quux(){
+        System.out.println("quux called");
+    }
+}
